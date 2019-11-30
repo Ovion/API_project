28a648f991c857c8a10812e0bc75631e45894dc5
Ovion <ovi2825@gmail.com>
2019-11-30 13:37:11 +0100

    Cambios

--- src/mysql/mysql.py
+++ src/mysql/mysql.py
@@ -13,47 +13,47 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema api_project
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `api_project` ;
-USE `api_project` ;
+CREATE SCHEMA IF NOT EXISTS api_project ;
+USE api_project ;
 
 -- -----------------------------------------------------
 -- Table `api_project`.`channels`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `api_project`.`channels` (
-  `id_channels` INT NOT NULL,
-  PRIMARY KEY (`id_channels`))
+CREATE TABLE IF NOT EXISTS api_project.channels (
+  id_channels INT NOT NULL,
+  PRIMARY KEY (id_channels))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `api_project`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `api_project`.`users` (
-  `id_users` INT NOT NULL,
-  `user_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_users`))
+CREATE TABLE IF NOT EXISTS api_project.users (
+  id_users INT NOT NULL,
+  user_name VARCHAR(45) NULL,
+  PRIMARY KEY (id_users))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `api_project`.`messages`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `api_project`.`messages` (
-  `id_messages` INT NOT NULL,
-  `users_id_users` INT NOT NULL,
-  `channels_id_channels` INT NOT NULL,
-  `text` VARCHAR(255) NULL,
-  PRIMARY KEY (`id_messages`),
-  INDEX `fk_messages_users_idx` (`users_id_users` ASC) VISIBLE,
-  INDEX `fk_messages_channels1_idx` (`channels_id_channels` ASC) VISIBLE,
-  CONSTRAINT `fk_messages_users`
-    FOREIGN KEY (`users_id_users`)
-    REFERENCES `api_project`.`users` (`id_users`)
+CREATE TABLE IF NOT EXISTS api_project.messages (
+  id_messages INT NOT NULL,
+  users_id_users INT NOT NULL,
+  channels_id_channels INT NOT NULL,
+  text VARCHAR(255) NULL,
+  PRIMARY KEY (id_messages),
+  INDEX fk_messages_users_idx (users_id_users ASC) VISIBLE,
+  INDEX fk_messages_channels1_idx (channels_id_channels ASC) VISIBLE,
+  CONSTRAINT fk_messages_users
+    FOREIGN KEY (users_id_users)
+    REFERENCES api_project.users (id_users)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_channels1`
-    FOREIGN KEY (`channels_id_channels`)
-    REFERENCES `api_project`.`channels` (`id_channels`)
+  CONSTRAINT fk_messages_channels1
+    FOREIGN KEY (channels_id_channels)
+    REFERENCES api_project.channels (id_channels)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
