0ee477d37a4b6abdec6b4abec0a30d2e9b473dc6
Ovion <ovi2825@gmail.com>
2019-12-02 06:14:34 +0100

    Funciona

--- src/Mongo.py
+++ src/Mongo.py
@@ -6,17 +6,43 @@ class ConectColl:
     def __init__(self):
         self.client = MongoClient(MONGO.format(USER, KEY))
         self.db = self.client['Slack']
-        self.coll = self.db['Chats']
+        self.chats = self.db['Chats']
+        self.users = self.db['Users']
 
-    def add_doc(self, docu):
-        aux = self.coll.insert_one(docu)
+    def add_doc_chats(self, docu):
+        aux = self.chats.insert_one(docu)
         print("Inserted: ", aux.inserted_id)
         return aux.inserted_id
 
-    def add_chat(self, user, channel, text):
+    def add_doc_users(self, docu):
+        aux = self.users.insert_one(docu)
+        print("Inserted: ", aux.inserted_id)
+        return aux.inserted_id
+
+    def last_id_user(self):
+        return list(self.users.find({}, {"id_user": 1, "_id": 0}).sort([('id_user', -1)]).limit(1))[0]['id_user']
+
+    def last_id_chat(self):
+        return list(self.chats.find({}, {"id_chat": 1, "_id": 0}).sort([('id_chat', -1)]).limit(1))[0]['id_chat']
+
+    def add_chat(self, id_user, channel, text):
+        user = list(self.users.find({"id_user": int(id_user)}))[0]["user"]
+        id_chat = self.last_id_chat()+1
+        # id_chat = 1
         document = {
             'user': user,
+            'id_user': id_user,
             'channel': channel,
-            'text': text
+            'text': text,
+            'id_chat': id_chat
         }
-        return self.add_doc(document)
+        return self.add_doc_chats(document), id_chat
+
+    def add_user(self, user):
+        id_user = self.last_id_user()+1
+        # id_user = 1
+        document = {
+            "id_user": id_user,
+            "user": user
+        }
+        return self.add_doc_users(document), id_user
--- src/api.py
+++ src/api.py
@@ -2,13 +2,25 @@ from bottle import route, run, get, post, request
 from Mongo import ConectColl
 
 
+@post('/user/new')
+def add_user():
+    user = request.forms.get('user')
+    _id, id_user = coll.add_user(user)
+    return {
+        "inserted_doc": str(_id),
+        "user_id": id_user
+    }
+
+
 @post('/conversation/new')
 def add_conver():
-    user = request.forms.get('user')
+    id_user = int(request.forms.get('id_user'))
     channel = request.forms.get('channel')
     text = request.forms.get('text')
+    _id, id_chat = coll.add_chat(id_user, channel, text)
     return{
-        'inserted_doc': str(coll.add_chat(user, channel, text))
+        'inserted_doc': str(_id),
+        'chat_id': id_chat
     }
 
 
